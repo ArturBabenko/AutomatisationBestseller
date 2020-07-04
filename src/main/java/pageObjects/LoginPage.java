a57ed2173fbc9db960830c72f6fadb7e1a9939b1
artur <arturo.babenko@gmail.com>
2020-07-04 18:29:20 +0200

    Test scenario and PageObject edited due to new tests created

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -18,6 +18,8 @@ public class LoginPage {
     private By doNotRememberPass = By.xpath("//a[@class='login__link-passwordreset']");
     private By isLogin = By.xpath("//a[@title='Wyloguj się']");
     private String expectedMsg = "Niestety, to nie pasuje do naszych rekordów. Sprawdź pisownię i spróbuj ponownie.";
+    private By emptyField = By.xpath("//span[contains(text(),'To pole jest wymagane.')]");
+    private String emptyErrMsg = "To pole jest wymagane.";
 
 
     public LoginPage(WebDriver driver) {
@@ -31,24 +33,21 @@ public class LoginPage {
     public WebElement passwordInput() {
         return driver.findElement(passwordField);
     }
-
     public WebElement rememberBox(){
         return driver.findElement(remember);
     }
-
     public WebElement loginButton(){
         return driver.findElement(loginButton);
     }
-
     public String loginError(){
         return driver.findElement(loginError).getText();
     }
-
     public WebElement isLogin() {return driver.findElement(isLogin);}
-
     public String expectedMsg() {return expectedMsg;}
-
-
+    public String emptyField(){
+        return driver.findElement(emptyField).getText();
+    }
+    public String emptyErrMsg() {return emptyErrMsg;}
     public ForgotPassword forgotPassword() {
         driver.findElement(doNotRememberPass).click();
         ForgotPassword fp = new ForgotPassword(driver);
