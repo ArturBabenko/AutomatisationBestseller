b735f6dad13cf73ba0cf07c305419f95df349222
artur <arturo.babenko@gmail.com>
2020-07-11 19:00:43 +0200

    New tests created

---
+++ src/test/java/com/BS0207SearchSpecChar.java
@@ -0,0 +1,37 @@
+package com;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import pageObjects.LandingPage;
+import pageObjects.SearchPage;
+import resources.Skeleton;
+
+import java.io.IOException;
+
+
+public class BS0207SearchSpecChar extends Skeleton {
+    public WebDriver driver;
+    //public static Logger log = LogManager.getLogger(Skeleton.class.getName());
+
+    @Test
+    public void itemNonExistItemNumberSearch() throws IOException {
+        //WebDriverWait wdw = new WebDriverWait(driver, 6);
+        driver = seleniumDriver();
+        driver.manage().window().maximize();
+        driver.get(properties.getProperty("url"));
+        LandingPage lp = new LandingPage(driver);
+        lp.popupCookie().click();
+        SearchPage searchPage = lp.searchChoose();
+        searchPage.searchField().sendKeys(searchPage.specChars());
+        searchPage.checkoutCart().click();
+        //wdw.until(ExpectedConditions.visibilityOf(searchPage.clothNameFiltr()));
+        Assert.assertTrue(searchPage.errorMsg().isDisplayed());
+    }
+
+    @AfterTest
+    public void closeWindow() {
+        driver.close();
+    }
+}
